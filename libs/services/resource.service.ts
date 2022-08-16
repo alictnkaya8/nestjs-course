a64429e06a932b566dadeefc76dac1fd749f4e37
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-16 16:38:39 +0300

    custom query added for findall query

--- libs/services/resource.service.ts
+++ libs/services/resource.service.ts
@@ -1,9 +1,19 @@
 import { Model } from "mongoose";
 import { AuditModel } from "tools/models/audit.model";
+import { FilterModel } from "tools/models/filter.model";
 
 export class ResourceService<T extends any, C extends Object, U extends Object> {
 
-    constructor(protected readonly mongoModel: Model<T>) {}
+    generalSearchQuery = {
+        page: 1,
+        size: 10,
+        sort: 'ASC',
+        sortBy: '_id',
+        queryText: '',
+        searchBy: 'name'
+    };
+
+    constructor(protected readonly mongoModel: Model<T>) { }
 
     async create(model: C): Promise<T> {
         const audit = new AuditModel();
@@ -15,8 +25,27 @@ export class ResourceService<T extends any, C extends Object, U extends Object> 
         return await createdModel.save();
     }
 
-    async findAll(): Promise<T[]> {
-        return await this.mongoModel.find().exec();
+    async findAll(query?: FilterModel): Promise<any[]> {
+        if (Object.keys(query).length !== 0) {
+            const searchValue = await { ...this.generalSearchQuery, ...query };
+            const userRegex = new RegExp(searchValue.queryText, 'i');
+
+            return await this.mongoModel
+                .find({ searchValue })
+                .limit(Math.max(0, searchValue.size))
+                .skip(searchValue.size * (searchValue.page - 1))
+                .sort()
+                .exec();
+        } else {
+            const count = await this.mongoModel.countDocuments().exec();
+            const data = await this.mongoModel
+                .find()
+                .limit(Math.max(0, this.generalSearchQuery.size))
+                .skip(this.generalSearchQuery.size * (this.generalSearchQuery.page - 1))
+                .exec();
+
+            return await [{ success: true, size: this.generalSearchQuery.size, total: count, data }];
+        }
     }
 
     async findOne(id: string): Promise<T> {
