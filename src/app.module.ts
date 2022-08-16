bd37f49f9ff192ff0cd609fce893a9aee19f5c0a
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-16 11:14:52 +0300

    endpoints added app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,20 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LibsModule } from 'libs/libs.module';
+import { RoleModule } from './role/role.module';
+import { ProductModule } from './product/product.module';
+import { ActivityModule } from './activity/activity.module';
+import { GroupModule } from './group/group.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { TicketModule } from './ticket/ticket.module';
+import { ProductTypeModule } from './product/product-type/product-type.module';
+import { ActivityTypeModule } from './activity/activity-type/activity-type.module';
+import { InventoryTypeModule } from './inventory/inventory-type/inventory-type.module';
+import { TicketTypeModule } from './ticket/ticket-type/ticket-type.module';
 
 @Module({
-  imports: [UserModule, LibsModule, MongooseModule.forRoot(environment.mongoUrl)],
+  imports: [ UserModule, RoleModule, ProductModule, ActivityModule, GroupModule, InventoryModule, TicketModule, ProductTypeModule, ActivityTypeModule, InventoryTypeModule, TicketTypeModule, LibsModule, MongooseModule.forRoot(environment.mongoUrl)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
